1d3b57f6b7aaac0fe6c8f2264d8f0c1a47862ccf
kurtiz <papiliocurtis@gmail.com>
2023-05-26 08:33:37 +0000

    added main screen :lipstick:

---
+++ Screens/MainScreen.js
@@ -0,0 +1,44 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ReceiptFragment from "../Fragements/ReceiptFragment";
+import ExpensesFragment from "../Fragements/ExpensesFragment";
+import {useLayoutEffect} from "react";
+import {useNavigation} from "@react-navigation/native";
+import {Icon} from "@rneui/base"
+
+const Tab = createBottomTabNavigator();
+
+
+const MainScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false // hiding or disabling the default header from react-native
+        })
+    }, []);
+
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Receipts"
+                component={ReceiptFragment}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Icon  name="receipt" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Expenses"
+                component={ExpensesFragment}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Icon type="font-awesome" name="book" color={color} size={size} />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default MainScreen;
