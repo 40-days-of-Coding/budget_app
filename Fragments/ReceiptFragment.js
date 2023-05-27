f1d8ab51c73d046ecf8b2ae8f5c4a41540f9d75d
kurtiz <papiliocurtis@gmail.com>
2023-05-27 23:08:35 +0000

    Added new elements to the add receipt modal

--- Fragments/ReceiptFragment.js
+++ Fragments/ReceiptFragment.js
@@ -2,7 +2,7 @@ import {
     View,
     Text,
     TouchableOpacity,
-    ScrollView
+    ScrollView,
 } from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import React, {useLayoutEffect, useRef, useState} from "react";
@@ -10,13 +10,18 @@ import {SafeAreaView} from "react-native-safe-area-context";
 import {Icon} from "@rneui/base";
 import ReceiptComponent from "../Components/ReceiptComponent";
 import RBSheet from "@nonam4/react-native-bottom-sheet";
+import {TextInput, Button} from "react-native-paper";
 
 const ReceiptFragment = () => {
     const navigation = useNavigation();
-    const [modalVisible, setModalVisibility] = useState(false);
+    // states
+    const [donationValue, setDonationValue] = useState("Money");
+    const [paymentMethod, setPaymentMethod] = useState("Cash");
 
     // ref
     const refRBSheet = useRef(null);
+    const refDonationType = useRef(null);
+    const refPaymentMethod = useRef(null);
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -27,6 +32,7 @@ const ReceiptFragment = () => {
     return (
         <SafeAreaView>
             <View className="bg-white pt-3">
+                {/* Page Header */}
                 <View className="flex-row items-center space-x-2 border-gray-200 border-b pb-3">
                     <View className="flex-1">
                         <Text className="ml-5 text-2xl font-bold">
@@ -40,7 +46,10 @@ const ReceiptFragment = () => {
                         <Icon type="ionicon" name="add-outline" color="white"/>
                     </TouchableOpacity>
                 </View>
+                {/* End Page Header */}
             </View>
+
+            {/* Receipts */}
             <ScrollView>
                 <ReceiptComponent
                     name="Honourable Pharaoh"
@@ -100,12 +109,15 @@ const ReceiptFragment = () => {
                     amount="1009.00"
                 />
             </ScrollView>
+            {/* End Receipts */}
+
+            {/* Add Receipt Modal */}
             <RBSheet
                 ref={refRBSheet}
                 closeOnDragDown={true}
                 closeOnPressMask={false}
                 animationType="slide"
-                height={350}
+                height={450}
                 customStyles={{
                     container: {
                         borderTopLeftRadius: 20,
@@ -118,24 +130,215 @@ const ReceiptFragment = () => {
                 <View>
                     <View className="items-center text-center border-b border-gray-200">
                         <View className="flex-row items-center">
-                            <Text className="flex-1 font-bold text-center text-2xl py-3">
+                            <Text className="flex-1 font-bold ml-4 text-2xl py-3">
                                 Add Receipt
                             </Text>
                             <TouchableOpacity
                                 className="mr-3"
                                 onPress={() => refRBSheet.current?.close()}
                             >
-                                <Icon type="ionicon" color="blue" name="close-outline"/>
+                                <Icon type="ionicon" size={35} name="close-outline"/>
                             </TouchableOpacity>
                         </View>
                     </View>
                     <ScrollView>
-                        <View className="flex-1 h-72 rounded-t-xl bg-white">
-
+                        <View className="flex-1 rounded-t-xl bg-white">
+                            <TextInput
+                                className="mx-2 mb-1 bg-gray-50"
+                                placeholder="Name"
+                                label="Name"
+                                mode="outlined"
+                                outlineColor="gray"
+                                activeOutlineColor="gray"
+                            />
+                            <TextInput
+                                className="mx-2 mb-1 bg-gray-50"
+                                placeholder="Name"
+                                label="Donation Type"
+                                mode="outlined"
+                                value={donationValue}
+                                editable={false}
+                                right={
+                                    <TextInput.Icon
+                                        icon="chevron-down"
+                                        onPress={() => refDonationType.current?.open()}/>
+                                }
+                                outlineColor="gray"
+                                activeOutlineColor="gray"
+                            />
+                            {(donationValue === "Money") ?
+                                <>
+                                    <TextInput
+                                        className="mx-2 mb-1 bg-gray-50"
+                                        placeholder="Payment Type"
+                                        label="Payment Type"
+                                        mode="outlined"
+                                        outlineColor="gray"
+                                        activeOutlineColor="gray"
+                                        value={paymentMethod}
+                                        right={
+                                            <TextInput.Icon
+                                                icon="chevron-down"
+                                                onPress={() => refPaymentMethod.current?.open()}
+                                            />
+                                        }
+                                    />
+                                    <TextInput
+                                        className="mx-2 mb-1 bg-gray-50"
+                                        placeholder="Amount"
+                                        label="Amount"
+                                        mode="outlined"
+                                        keyboardType="numeric"
+                                        outlineColor="gray"
+                                        activeOutlineColor="gray"
+                                    />
+                                </> : <>
+                                    <TextInput
+                                        className="mx-2 mb-1 bg-gray-50"
+                                        placeholder="Item"
+                                        label="Item"
+                                        mode="outlined"
+                                        outlineColor="gray"
+                                        activeOutlineColor="gray"
+                                    />
+                                    <TextInput
+                                        className="mx-2 mb-1 bg-gray-50"
+                                        placeholder="Quantity / Size"
+                                        label="Quantity / Size"
+                                        mode="outlined"
+                                        outlineColor="gray"
+                                        activeOutlineColor="gray"
+                                    />
+                                </>
+                            }
+                            <TextInput
+                                className="mx-2 mb-1 bg-gray-50"
+                                placeholder="Address"
+                                label="Address"
+                                mode="outlined"
+                                outlineColor="gray"
+                                activeOutlineColor="gray"
+                            />
+                            <TouchableOpacity
+                                onPress={() => alert("sent")}
+                                className="mb-1 py-3 rounded-md px-3 mx-20 bg-blue-500" mode="contained">
+                                <Text className="text-white text-lg text-center">Save</Text>
+                            </TouchableOpacity>
                         </View>
                     </ScrollView>
                 </View>
             </RBSheet>
+            {/* End Add Receipt Modal */}
+
+            {/* Donation Type Modal */}
+            <RBSheet
+                ref={refDonationType}
+                closeOnDragDown={true}
+                closeOnPressMask={false}
+                animationType="slide"
+                height={150}
+                customStyles={{
+                    container: {
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                    },
+                    draggableIcon: {
+                        backgroundColor: "#000"
+                    }
+                }}>
+                <View>
+                    <View className="rounded-t-xl bg-white">
+                        <View>
+                            <TouchableOpacity
+                                onPress={
+                                    () => {
+                                        setDonationValue("Money");
+                                        refDonationType.current?.close()
+                                    }
+                                }
+                                className="mt-1 border-b border-t border-gray-200 py-3 px-20">
+                                <Text className="text-center text-lg">Money</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={
+                                    () => {
+                                        setDonationValue("Item");
+                                        refDonationType.current?.close()
+                                    }
+                                }
+                                className="mb-3 border-b border-gray-200 py-3 px-20">
+                                <Text className="text-center text-lg">Item</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </RBSheet>
+            {/* End Donation Type Modal */}
+
+            {/* Payment Method Modal */}
+            <RBSheet
+                ref={refPaymentMethod}
+                closeOnDragDown={true}
+                closeOnPressMask={false}
+                animationType="slide"
+                height={250}
+                customStyles={{
+                    container: {
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                    },
+                    draggableIcon: {
+                        backgroundColor: "#000"
+                    }
+                }}>
+                <View>
+                    <View className="rounded-t-xl bg-white">
+                        <View>
+                            <TouchableOpacity
+                                onPress={
+                                    () => {
+                                        setPaymentMethod("Cash");
+                                        refPaymentMethod.current?.close()
+                                    }
+                                }
+                                className="mt-1 border-b border-t border-gray-200 py-3 px-20">
+                                <Text className="text-center text-lg">Cash</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={
+                                    () => {
+                                        setPaymentMethod("Cheque");
+                                        refPaymentMethod.current?.close()
+                                    }
+                                }
+                                className="mb-3 border-b border-gray-200 py-3 px-20">
+                                <Text className="text-center text-lg">Cheque</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={
+                                    () => {
+                                        setPaymentMethod("Credit Card");
+                                        refPaymentMethod.current?.close()
+                                    }
+                                }
+                                className="mb-3 border-b border-gray-200 py-3 px-20">
+                                <Text className="text-center text-lg">Credit Card</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={
+                                    () => {
+                                        setPaymentMethod("Mobile Money");
+                                        refPaymentMethod.current?.close()
+                                    }
+                                }
+                                className="mb-3 border-b border-gray-200 py-3 px-20">
+                                <Text className="text-center text-lg">Mobile Money</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </RBSheet>
+            {/* End Payment Method Modal */}
         </SafeAreaView>
     );
 };
